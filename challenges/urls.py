27826cff9157357a5790db40a679fb1944710ca9
Your Name <you@example.com>
2025-05-06 13:16:19 +0200

    Dynamic path used for months

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('january', views.january_view)
+    path('<int:month>', views.monthly_challenge_by_number),
+    path('<str:month>', views.monthly_challenge),
+    
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def january_view(request):
-    return HttpResponse("Hello world")
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
+
+def monthly_challenge(request, month):
+    challenge_text = None
+
+    if month == 'january':
+        challenge_text = "Write plans for starting year"
+    elif month == 'february':
+        challenge_text = "Walk at least 30 minutes per day"
+    elif month == 'march':
+        challenge_text = "Read book at leat 20 minutes per day"
+    elif month == 'april':
+        challenge_text = "Walk at least 30 minutes per day"
+    elif month in ['may','june', 'july']:
+        challenge_text = "Reduce weight with 1 kilogram"
+    elif month == 'august':
+        challenge_text = "Go on holiday"
+    elif month in ['september', 'october', 'november', 'december']:
+        challenge_text = "Maintain weight and read book at least 20 minutes per day"
+    else:
+        return HttpResponseNotFound("Wrong month")
+    return HttpResponse(challenge_text)
